02a90247b5156edfb6fd392a13d27365cda51f97
ilyaarhipov <ilyaarhipov228@gmail.com>
2023-05-04 12:43:23 +0500

    feat: added data generation for photo description

--- js/main.js
+++ js/main.js
@@ -1,13 +1,63 @@
+const DESCRIPTIONS = [
+  'Я на море',
+  'Отдыхаю',
+  'Вкусно кушаю',
+  'Не каждый волк, кто воет',
+  'Красивое описание',
+  'С котанами',
+  'Живу лучшую из 9 жизней',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Артём',
+  'Мишаня',
+  'Софа',
+  'Киборг Убийца',
+  'Мафиозник',
+  'Николя',
+  'Санек',
+  'Иришка',
+  'Люций',
+  'Костян',
+  'Роман',
+  'Кира',
+];
+
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
 function getRandomIntInclusive(min, max) {
-  if (min > -1 && max > -1) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  }
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-const checkTheLengthOfTheString = (verifiableString, maxLength) => verifiableString.length < maxLength + 1;
+const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length-1)];
 
-getRandomIntInclusive(0,5);
+const createDescription = () => ({
+  id: getRandomIntInclusive(1, 25),
+  url: `photos/${  getRandomIntInclusive(1,25)  }.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomIntInclusive(15, 200),
+  comment: {
+    id: getRandomIntInclusive(1,1000),
+    avatar: `img/avatar-${  getRandomIntInclusive(1,6)  }.svg`,
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES),
+  },
+});
 
-checkTheLengthOfTheString('qwerty', 10);
+const simmularDescriptions = Array.from({length:25}, createDescription);
+
+simmularDescriptions();
+
+checkStringLength('sdfgh', 10);
