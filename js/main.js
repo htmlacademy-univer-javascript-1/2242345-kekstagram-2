ea0b650ac60974b11f362e83ff5824b337ed09ea
ilyaarhipov <ilyaarhipov228@gmail.com>
2023-05-10 15:32:13 +0500

    feat: fix error eslint

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 import './util.js';
-import { generatePhotos } from './data.js';
+// import { generatePhotos } from './data.js';
 import './prewiew.js';
 import './photo.js';
 
-// generatePhotos();
-// console.log(generatePhotos);
